7ef39474ef17dd2ed4b17167c0e1962d7b0136cc
DominikRousa <145441116+DominikRousa@users.noreply.github.com>
2024-01-16 12:54:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const last = ["circle"]
 
 function setBlock(value){
-    
-    if(last == "circle"){
-        document.getElementById(value).innerHTML = "X";
-        last.length = 0
-        last.push("cross")
-    } else if(last == "cross") {
-        document.getElementById(value).innerHTML = "O";
-        last.length = 0
-        last.push("circle")
+
+    if( document.getElementById(value).innerHTML.trim() == ""|| document.getElementById(value).innerHTML.trim() == "" ){
+        if(last == "circle"){
+            document.getElementById(value).innerHTML = "X";
+            last.length = 0
+            last.push("cross")
+        } else if(last == "cross") {
+            document.getElementById(value).innerHTML = "O";
+            last.length = 0
+            last.push("circle")
+        }
     }
+    
+}
+
+function resetall(){
+    document.getElementById("a").innerHTML = "";
+    document.getElementById("b").innerHTML = "";
+    document.getElementById("c").innerHTML = "";
+    document.getElementById("d").innerHTML = "";
+    document.getElementById("e").innerHTML = "";
+    document.getElementById("f").innerHTML = "";
+    document.getElementById("g").innerHTML = "";
+    document.getElementById("h").innerHTML = "";
+    document.getElementById("i").innerHTML = "";
 }
